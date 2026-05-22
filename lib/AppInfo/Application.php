6324175ef5eaece82e91d6461ae613cc99e5d1bf
James Manuel <moodyjmz@users.noreply.github.com>
2026-05-22 23:43:01 +0200

    ✨ feat: register TokenManager in DI container with session user injection
    Signed-off-by: James Manuel <moodyjmz@users.noreply.github.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace OCA\Office\AppInfo;
 
+use OCA\Office\TokenManager;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\EventDispatcher\IEventDispatcher;
+use OCP\Files\IRootFolder;
+use OCP\IURLGenerator;
+use OCP\IUserSession;
+use Psr\Log\LoggerInterface;
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'office';
@@ -18,6 +24,16 @@ class Application extends App implements IBootstrap {
 	}
 
 	public function register(IRegistrationContext $context): void {
+		$context->registerService(TokenManager::class, static function ($c) {
+			return new TokenManager(
+				$c->get(IRootFolder::class),
+				$c->get(\OCA\Office\Db\WopiMapper::class),
+				$c->get(IURLGenerator::class),
+				$c->get(IEventDispatcher::class),
+				$c->get(LoggerInterface::class),
+				$c->get(IUserSession::class)->getUser()?->getUID(),
+			);
+		});
 	}
 
 	public function boot(IBootContext $context): void {
