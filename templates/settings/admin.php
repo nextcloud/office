d18761b676991efaa929cbbb7c54dd835a4f4c5c
James Manuel <moodyjmz@users.noreply.github.com>
2026-05-22 23:47:33 +0200

    ✨ feat: add admin settings PHP template
    Signed-off-by: James Manuel <moodyjmz@users.noreply.github.com>

---
+++ templates/settings/admin.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use OCA\Office\AppInfo\Application;
+use OCP\Util;
+
+/**
+ * @var array $_ Template variables:
+ *   - wopi_url                      string  Editor server base URL
+ *   - disable_certificate_verification string  'yes' or 'no'
+ */
+
+Util::addScript(Application::APP_ID, Application::APP_ID . '-settings-admin');
+
+?>
+<script id="office-admin-data" type="application/json">
+<?php echo json_encode([
+	'wopi_url' => $_['wopi_url'],
+	'disable_certificate_verification' => $_['disable_certificate_verification'],
+], JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT); ?>
+</script>
+<div id="office-settings-admin"></div>
