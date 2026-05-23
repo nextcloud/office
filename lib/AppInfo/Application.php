d0b05749c6dacde16370005b565cf1404c1bacfd
James Manuel <moodyjmz@users.noreply.github.com>
2026-05-23 08:07:10 +0200

    🔧 chore: move background-jobs and settings registration to info.xml
    NC35 removed registerJob() and registerSettings() from
    IRegistrationContext. Declare the CleanupJob background job and the
    Admin settings class in appinfo/info.xml instead, which is the
    supported mechanism from NC35 onwards.
    
    Signed-off-by: James Manuel <moodyjmz@users.noreply.github.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace OCA\Office\AppInfo;
 
-use OCA\Office\BackgroundJob\CleanupJob;
-use OCA\Office\Settings\Admin;
 use OCA\Office\TokenManager;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
@@ -26,9 +24,6 @@ class Application extends App implements IBootstrap {
 	}
 
 	public function register(IRegistrationContext $context): void {
-		$context->registerSettings(Admin::class);
-		$context->registerJob(CleanupJob::class);
-
 		$context->registerService(TokenManager::class, static function ($c) {
 			return new TokenManager(
 				$c->get(IRootFolder::class),
